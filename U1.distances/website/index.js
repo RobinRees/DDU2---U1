2feaae41acd9ff69e381d73cd1134250fb9c39c3
RobinRees <Reehees12@gmail.com>
2024-11-20 14:46:38 +0100

    Strukturen byggd

--- U1.distances/website/index.js
+++ U1.distances/website/index.js
@@ -1,4 +1,15 @@
 // Recommended: All functions declared here
+let table = document.querySelector("#table");
+const emptyCell = document.createElement("div");
+emptyCell.className = "cell";
+table.appendChild(emptyCell);
+
+for (let i = 0; i < cities.length; i++) {
+    const createDiv2 = document.createElement("div");
+    table.appendChild(createDiv2);
+    createDiv2.className ="cell";
+    createDiv2.textContent = i;
+}
 
 const cityDiv = document.querySelector("#cities");
 let cityPrompt = prompt("Skriv en stad i Europa");
