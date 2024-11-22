d6af16ed7a547078cc613ac4f057b078da85ea7e
RobinRees <Reehees12@gmail.com>
2024-11-22 13:20:43 +0100

    Skrivit funktion

--- U1.distances/website/index.js
+++ U1.distances/website/index.js
@@ -14,41 +14,85 @@ for (let i = 0; i < cities.length; i++) {
 const cityDiv = document.querySelector("#cities");
 let cityPrompt = prompt("Skriv en stad i Europa");
 let h2Text = document.querySelector("h2");
-h2Text.innerHTML = cityPrompt;
 
 
 
-function createAllCityBoxes () {
-    for(let city of cities) {
-        const createDiv = document.createElement("div");
-        cityDiv.appendChild(createDiv);
-        createDiv.className = "cityBox";
-        createDiv.textContent = `${city.name}`
-        if (cityPrompt === city.name) {
+
+
+for(let city of cities) {
+    const createDiv = document.createElement("div");
+    cityDiv.appendChild(createDiv);
+    createDiv.className = "cityBox";
+    createDiv.textContent = `${city.name}`
+}
+findClosestAndFurthest(cityPrompt);
+
+function findClosestAndFurthest(cityPrompt) {
+    let inputCityId = null;
+
+    for (const city of cities) {
+        if (city.name.toLocaleLowerCase() === cityPrompt.toLocaleLowerCase()) {
+            inputCityId = city.id;
             h2Text.innerHTML = `${cityPrompt} (${city.country})`;
-            createDiv.className = "target cityBox";
+            break;
         }
-        
     }
-};
-createAllCityBoxes();
 
+    if (inputCityId === null) {
+        h2Text.innerHTML = `${cityPrompt} finns inte i databasen;`
+    }
 
+    let closestCity = null;
+    let furthestCity = null;
+    let minDistance = Infinity;
+    let maxDistance = null;
 
+    for (const distance of distances) {
+        let otherCityId = null;
+
+        if (distance.city1 === inputCityId) {
+            otherCityId = distance.city2;
+        } else if (distance.city2 === inputCityId) {
+            otherCityId = distance.city1;
+        }
+
+        if (otherCityId === null) continue;
+
+        if (distance.distance < minDistance) {
+            minDistance = distance.distance;
+            closestCity = otherCityId;
+        }
+
+        if (distance.distance > maxDistance) {
+            maxDistance = distance.distance;
+            furthestCity = otherCityId;
+        }
+    }
+
+    let closestCityName = null;
+    let furthestCityName = null;
+
+    for (const city of cities) {
+        if (city.id === closestCity) {
+            closestCityName = city.name;
+        }
+        if (city.id === furthestCity) {
+            furthestCityName = city.name;
+        }
+    }
+
+    h2Text.innerHTML = 
+    `Du skrev: ${cityPrompt} <br>
+    Längst bort är: ${furthestCityName} (${maxDistance} km) <br>
+    Närmast är: ${closestCityName} (${minDistance} km)`
+;
+
+}
+findClosestAndFurthest(cityPrompt);
+/*
 
 // Recommended: constants with references to existing HTML-elements
 
 // Recommended: Ask for the city name and then the rest of the code
 
-/*const table = document.querySelector("#table");
-
-for(let city of cities) {
-    const createDiv = document.createElement("div");
-    table.appendChild(createDiv);
-    createDiv.textContent = `${city.id} - ${city.name}`;
-    for(let distance of distances) {
-        
-    }
-    
-}
-    */
+/*const table = document.querySelector("#table");*/
